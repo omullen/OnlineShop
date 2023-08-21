6d4be6bc1f51009898a10198609300bb3049c8d7
omullen <lavendericana@gmail.com>
2023-08-21 21:17:55 +0300

    initial bootstrap/style changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
+    <Header />
     <h1>Welcome to my shop!</h1>
+    <Footer />
     </>
   );
 }
